e0fb92d6d98aafed26a3535be6720675e3c23727
Ashbeel Ghouri <ash.ghouri@techtronix.biz>
2020-12-12 23:07:56 +0500

    adding the property binding on element app-card in app.components and receiving these properties via Input in the component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'card-posts';
+  posts = [{
+    title: 'Neat Tree',
+    image_url: 'assets/tree.jpeg',
+    username: 'nature',
+    content: 'I saw this neat tree today'
+  }, {
+    title: 'Snowy Mountain',
+    image_url: 'assets/mountain.jpeg',
+    username: 'mountainlover',
+    content: 'Here is picture of snowy mountain'
+  }, {
+    title: 'Mountain Biking',
+    image_url: 'assets/biking.jpeg',
+    username: 'biking222',
+    content: 'I done some biking today'
+  }];
 }
